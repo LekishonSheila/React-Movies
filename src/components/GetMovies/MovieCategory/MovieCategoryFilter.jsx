c3d902d4b33c91c9f231915e163296be3b916666
Lekishon <lekishonsheila@gmail.com>
2023-07-31 09:41:30 +0300

    react

--- src/components/GetMovies/MovieCategory/MovieCategoryFilter.jsx
+++ src/components/GetMovies/MovieCategory/MovieCategoryFilter.jsx
@@ -1,28 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CategoryFilter = ({ selectedCategory, handleCategoryChange }) => {
-  const categories = [
-    { id: "all", name: "All" },
-    { id: "28", name: "Action" },
-    { id: "12", name: "Adventure" },
-    { id: "16", name: "Animation" },
-    { id: "35", name: "Comedy" },
-    { id: "80", name: "Crime" },
-    { id: "99", name: "Documentary" },
-    { id: "18", name: "Drama" },
-    { id: "10751", name: "Family" },
-    { id: "14", name: "Fantasy" },
-    { id: "36", name: "History" },
-    { id: "27", name: "Horror" },
-    { id: "10402", name: "Music" },
-    { id: "9648", name: "Mystery" },
-    { id: "10749", name: "Romance" },
-    { id: "878", name: "Science Fiction" },
-    { id: "10770", name: "TV Movie" },
-    { id: "53", name: "Thriller" },
-    { id: "10752", name: "War" },
-    { id: "37", name: "Western" },
-  ];
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+   
+    fetch("https://example.com/api/categories")
+      .then((response) => response.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error("Error fetching categories:", error));
+  }, []);
 
   return (
     <div className="category-buttons">
